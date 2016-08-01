3718dbe0cade01cc146601fcd94de54a8b4e45df
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2016-08-01 11:23:04 +0200

    Several test scenarios showing the shortcomings in dotConnect's implementation of EF.Core provider

---
+++ Tests/DevArt.Tests.dotConnect/Entities.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DevArt.Tests.dotConnect
+{
+    public class Folder
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+        public User Owner { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LongDescription { get; set; }
+    }
+
+    public class TestDbContext : DbContext
+    {
+        private readonly Action<DbContextOptionsBuilder> configure;
+
+        public TestDbContext(Action<DbContextOptionsBuilder> configure)
+        {
+            this.configure = configure;
+        }
+
+        public TestDbContext(string connectionString)
+            : this(builder => builder.UseOracle(connectionString))
+        {
+        }
+
+        public TestDbContext(DbConnection dbConnection)
+            : this(builder => builder.UseOracle(dbConnection))
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            this.configure(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var folder = modelBuilder.Entity<Folder>();
+            folder.HasKey(f => f.Id);
+            folder.Property(f => f.Id).ValueGeneratedOnAdd();
+            folder.Property(f => f.Name).IsRequired().HasMaxLength(100);
+            folder
+                .HasOne(f => f.Owner)
+                .WithMany()
+                .HasForeignKey(f => f.OwnerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            var user = modelBuilder.Entity<User>();
+            user.HasKey(u => u.Id);
+            user.Property(u => u.Id).ValueGeneratedOnAdd();
+            user.Property(u => u.Name).IsRequired().HasMaxLength(100).IsConcurrencyToken();
+            user.Property(u => u.LongDescription);
+        }
+    }
+}
---
+++ Tests/DevArt.Tests.dotConnect/UnitTest.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Linq;
+using Devart.Data.Oracle;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevArt.Tests.dotConnect
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private const string ConnectionString = "User Id=pvs_c;Password=ic1;Server=demo11g;Direct=True;Sid=emp";
+
+        [TestMethod]
+        public void DbContext_Select_Where_ClobField()
+        {
+            using (var dbContext = new TestDbContext(ConnectionString))
+            {
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+
+                // Works fine for varchar
+                dbContext.Set<User>().SingleOrDefault(u => u.Name== "test");
+
+                // Fails for clob
+                dbContext.Set<User>().SingleOrDefault(u => u.LongDescription == "test");
+            }
+        }
+
+        [TestMethod]
+        public void DbContext_Include_OrderBy_First()
+        {
+            using (var dbContext = new TestDbContext(ConnectionString))
+            {
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+
+                var user = new User { Name = "John" };
+                var folder = new Folder { Name = "test", Owner = user };
+
+                dbContext.Add(folder);
+                dbContext.SaveChanges();
+            }
+
+            // Works fine without .OrderBy
+            using (var dbContext = new TestDbContext(ConnectionString))
+            {
+                dbContext.Set<Folder>()
+                    .Include(f => f.Owner)
+                    .First();
+            }
+
+            // Works fine without .First
+            using (var dbContext = new TestDbContext(ConnectionString))
+            {
+                dbContext.Set<Folder>()
+                    .Include(f => f.Owner)
+                    .OrderBy(f => f.Id)
+                    .ToList();
+            }
+
+            // Fails with .OrderBy.First
+            using (var dbContext = new TestDbContext(ConnectionString))
+            {
+                dbContext.Set<Folder>()
+                    .Include(f => f.Owner)
+                    .OrderBy(f => f.Id)
+                    .First();
+            }
+        }
+
+        [TestMethod]
+        public void DbContext_With_DbConnection_EnsureDeleted()
+        {
+            // Works fine with .UseOracle(ConnectionString)
+            using (var dbContext = new TestDbContext(ConnectionString))
+            {
+                dbContext.Database.EnsureDeleted();
+            }
+
+            // Fails with .UseOracle(DbConnection)
+            using (var dbConnection = new OracleConnection { ConnectionString = ConnectionString })
+            {
+                using (var dbContext = new TestDbContext(dbConnection))
+                {
+                    dbContext.Database.EnsureDeleted();
+                }
+            }
+        }
+
+        [TestMethod]
+        public void DbContext_With_DbConnection_Where_StringField_Equals_Property()
+        {
+            // Prepare the database with .UseOracle(ConnectionString), because .UseOracle(Connection) is broken.
+            using (var dbContext = new TestDbContext(ConnectionString))
+            {
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+
+                // Works fine with .UseOracle(ConnectionString)
+                dbContext.Set<User>().Where(u => u.Name == Environment.UserName).ToList();
+            }
+
+            using (var dbConnection = new OracleConnection { ConnectionString = ConnectionString })
+            {
+                using (var dbContext = new TestDbContext(dbConnection))
+                {
+                    // Works fine with string literals
+                    dbContext.Set<User>().Where(u => u.Name == "Test").ToList();
+
+                    // Fails with const property
+                    dbContext.Set<User>().Where(u => u.Name == Environment.UserName).ToList();
+                }
+            }
+        }
+
+        [TestMethod]
+        public void DbContext_With_DbConnection_SaveChanges()
+        {
+            // Prepare the database with .UseOracle(ConnectionString), because .UseOracle(Connection) is broken.
+            using (var dbContext = new TestDbContext(ConnectionString))
+            {
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+
+                // Works fine with .UseOracle(ConnectionString)
+                var user = new User { Name = "Oliver" };
+                dbContext.Add(user);
+                dbContext.SaveChanges();
+            }
+
+            // Fails with .UseOracle(DbConnection)
+            using (var dbConnection = new OracleConnection { ConnectionString = ConnectionString })
+            {
+                using (var dbContext = new TestDbContext(dbConnection))
+                {
+                    var user = new User { Name = "John" };
+                    dbContext.Add(user);
+                    dbContext.SaveChanges();
+                }
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DbUpdateConcurrencyException))]
+        public void DbContext_DbUpdateConcurrencyException()
+        {
+            using (var dbContext = new TestDbContext(ConnectionString))
+            {
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+
+                var user = new User { Name = "John" };
+                dbContext.Add(user);
+                dbContext.SaveChanges();
+
+                using (var dbContext2 = new TestDbContext(ConnectionString))
+                {
+                    User user2 = dbContext2.Set<User>().Single(u => u.Name == "John");
+                    user2.Name = "Oliver";
+                    dbContext2.SaveChanges();
+                }
+
+                user.LongDescription = "hello";
+                dbContext.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public void DbContext_With_LoggerProvider()
+        {
+            using (var dbContext = new TestDbContext(
+                builder =>
+                {
+                    builder.UseOracle(ConnectionString);
+
+                    var loggerFactory = new LoggerFactory();
+                    loggerFactory.AddProvider(new NullLoggerProvider());
+                    builder.UseLoggerFactory(loggerFactory);
+                }))
+            {
+                dbContext.Database.EnsureDeleted();
+                dbContext.Database.EnsureCreated();
+            }
+        }
+
+        private class NullLoggerProvider : ILoggerProvider
+        {
+            public ILogger CreateLogger(string categoryName) => new NullLogger();
+
+            public void Dispose() { }
+
+            private class NullLogger : ILogger
+            {
+                public bool IsEnabled(LogLevel logLevel) => true;
+
+                public void Log<TState>(
+                    LogLevel logLevel,
+                    EventId eventId,
+                    TState state,
+                    Exception exception,
+                    Func<TState, Exception, string> formatter)
+                {
+                }
+
+                public IDisposable BeginScope<TState>(TState state) => null;
+            }
+        }
+    }
+}
