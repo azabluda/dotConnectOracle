b9022ee32e9475ef113129a1726b137980d79e0f
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-11-03 12:48:25 +0100

    Temporary workaround for https://github.com/aspnet/Tooling/issues/456 "Test runner should pick up environment variables from launchSettings.json" Inspired by https://stackoverflow.com/a/43951218

--- Tests/DevArt.Tests.dotConnect/UnitTest.cs
+++ Tests/DevArt.Tests.dotConnect/UnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Devart.Data.Oracle;
 using Devart.Data.Oracle.Entity;
@@ -11,6 +12,8 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DevArt.Tests.dotConnect
 {
@@ -22,6 +25,37 @@ namespace DevArt.Tests.dotConnect
         private string ConnectionString =>
             Environment.GetEnvironmentVariable(ConnectionStringVarName)
             ?? throw new Exception($"Environment variable '{ConnectionStringVarName}' must be defined, e.g. in the Debug section of the project properties.");
+
+        // https://github.com/aspnet/Tooling/issues/456
+        // Test runner should pick up environment variables from launchSettings.json
+        [ClassInitialize]
+        public static void LaunchSettingsWorkaround(TestContext context)
+        {
+            const string launchSettingsJson = @"Properties\launchSettings.json";
+            if (!File.Exists(launchSettingsJson))
+            {
+                return;
+            }
+
+            using (var file = File.OpenText(launchSettingsJson))
+            {
+                using (var reader = new JsonTextReader(file))
+                { 
+                    var variables = JObject.Load(reader)
+                        .GetValue("profiles")
+                        .SelectMany(profiles => profiles.Children())
+                        .SelectMany(profile => profile.Children<JProperty>())
+                        .Where(prop => prop.Name == "environmentVariables")
+                        .SelectMany(prop => prop.Value.Children<JProperty>())
+                        .ToList();
+
+                    foreach (var variable in variables)
+                    {
+                        Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+                    }
+                }
+            }
+        }
 
         [TestMethod]
         public void DbContext_Select_Where_ClobField()
